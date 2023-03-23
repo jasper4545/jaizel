330a2f830e934cbdc566b71b481bdd2cb290a84e
jasper4545 <128397673+jasper4545@users.noreply.github.com>
2023-03-24 01:02:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,15 @@
 const audio = new Audio();
 audio.src = "kumpas.mp3";
 if(1 == 1) audio.play();
+console.log("heloo");const btn =document.getElementsByClassName("btn");
+
+const audio =new Audio("kumpas.mp3");
+let a =0
+addEventListener("click", ()=>{
+  a++;
+  console.log("hello");
+  if(a == 1) audio.play();
+  btn.remove;
+})
+
 console.log("heloo");
